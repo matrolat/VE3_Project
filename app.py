54055029b6e0f85e1ed7b7fe1514e060e5d26bba
Sameer Swankar <sameerswankar96@gmail.com>
2024-10-03 14:57:36 +0530

    requirement

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from flask_bcrypt import Bcrypt # type: ignore
 import os
 from flask import send_from_directory # type: ignore
 
-# Serve React App
-@app.route('/', defaults={'path': ''}) # type: ignore
-@app.route('/<path:path>') # type: ignore
-def serve(path):
-    if path != "" and os.path.exists("task-manager-frontend/build/" + path):
-        return send_from_directory('task-manager-frontend/build', path)
-    else:
-        return send_from_directory('task-manager-frontend/build', 'index.html')
+# # Serve React App
+# @app.route('/', defaults={'path': ''}) # type: ignore
+# @app.route('/<path:path>') # type: ignore
+# def serve(path):
+#     if path != "" and os.path.exists("task-manager-frontend/build/" + path):
+#         return send_from_directory('task-manager-frontend/build', path)
+#     else:
+#         return send_from_directory('task-manager-frontend/build', 'index.html')
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
